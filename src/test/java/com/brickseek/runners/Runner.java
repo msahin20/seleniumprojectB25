af8142bb55633e00216a756a2286445901899fa0
murat <mrtsahin79@gmail.com>
2022-06-18 22:21:21 -0400

    some change

--- src/test/java/com/brickseek/runners/Runner.java
+++ src/test/java/com/brickseek/runners/Runner.java
@@ -5,19 +5,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Runner {
-    static {
-
-        String [] test = {"1", "2"};
-        try {
-            BrickseekHotDeals.main(test);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    static {
+//
+//        String [] test = {"1", "2"};
+////        try {
+////            BrickseekHotDeals.main(test);
+////        } catch (InterruptedException e) {
+////            throw new RuntimeException(e);
+////        }
+//    }
 
     @Test
-    public void brickseektest (){
+    public void brickseektest () throws InterruptedException {
         for (int i = 0; i < 100; i++) {
+            String [] test1 = {"1", "2"};
+            BrickseekHotDeals.main(test1);
             System.out.println("hello");
             Assert.assertTrue(true);
         }
